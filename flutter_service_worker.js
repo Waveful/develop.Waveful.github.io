21a97dd9b9f0f5c7eeb98aef0bdbbeac7a48850e
Dolful <github-bot@waveful.app>
2022-02-24 01:06:26 +0000

    Update website

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {
 "/": "40c9fa3cd64e7da948d7f2cf2566e68f",
 "CNAME": "124e4042fbf152059b5805ece63fc59c",
 "app-ads.txt": "b345000f19c14c680aaaf0f9c105a221",
-"index.html": "392de73fe3847ae2a810e4c3ee147677",
+"index.html": "a38850626d2731482f685324b77d2bc3",
 "termsofuse/index.html": "40c9fa3cd64e7da948d7f2cf2566e68f",
 "version.json": "1fb178616608bf2fa0b1735a950e31a8",
 "icons/Icon-512.png": "b60bc846c8353d869dd35d48e276f0f0",
